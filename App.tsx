b1a9219a067527706106f08b983aa265723de0d3
Kunjal Soni <kunjal.s@simformsolutions.com>
2024-01-09 14:25:27 +0530

    implement mobx demo

--- App.tsx
+++ App.tsx
@@ -6,13 +6,30 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Home from './src/components/Home';
+import React, { useEffect } from "react";
+import { SafeAreaView } from "react-native";
+import Home from "./src/components/Home";
+import { getPersistedStore } from "mobx-persist-store";
+import { MovieStore, store } from "./src/store/MovieStore";
+
+const desiredStoreVersion = 1;
 
 const App = () => {
+  useEffect(() => {
+    const checkAndUpdateStoreVersion = async () => {
+      const persistedState: MovieStore | null = await getPersistedStore(
+        store.movieStore
+      );
+      if (persistedState && persistedState.version < desiredStoreVersion) {
+        store.movieStore.upgradeStoreVersion();
+      }
+    };
+
+    checkAndUpdateStoreVersion();
+  }, []);
+
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'lightgrey'}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "lightgrey" }}>
       <Home />
     </SafeAreaView>
   );
--- src/store/MovieStore.ts
+++ src/store/MovieStore.ts
@@ -1,23 +1,42 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { action, computed, makeObservable, observable } from "mobx";
+import { action, autorun, computed, makeObservable, observable } from "mobx";
 import { makePersistable } from "mobx-persist-store";
 
 export class Movie {
-  name: string;
+  name?: string;
+  newName?: string;
   year: string;
 }
 
-class MovieStore {
+export class MovieStore {
+  version = 1; //Initial version
   movies: Movie[];
 
   constructor() {
     this.movies = [];
-    makeObservable(this, { movies: observable, addMovie: action });
+
+    makeObservable(this, {
+      version: observable,
+      movies: observable,
+      addMovie: action,
+      upgradeStoreVersion: action,
+    });
+
     makePersistable(this, {
       name: "movies",
-      properties: ["movies"],
+      properties: ["movies", "version"],
       storage: AsyncStorage,
     });
+  }
+
+  upgradeStoreVersion() {
+    if (this.version == 1) {
+      // Migrate persisted data to Version 2
+      this.movies.forEach((movie) => {
+        movie.newName = movie.name;
+      });
+      this.version += 1;
+    }
   }
 
   addMovie(newMovie: Movie) {
